ded57310e301710640bd72ffa900c904e4ae68f1
Oleg <oleg@Matans-MacBook-Pro-5.local>
2023-09-06 01:23:24 +0300

    fixed balance problem

--- backend/test.ts
+++ backend/test.ts
@@ -1,8 +1,33 @@
 import { performance } from "perf_hooks";
 import supertest from "supertest";
 import { buildApp } from "./app";
+import { strict as assert } from 'node:assert';
 
 const app = supertest(buildApp());
+
+async function problemTest() {
+    console.log('Start testing problem');
+    await app.post("/reset").expect(204);
+    await Promise.all([
+        app.post("/charge").expect(200),
+        app.post("/charge").expect(200),
+        app.post("/charge").expect(200),
+        app.post("/charge").expect(200),
+        app.post("/charge").expect(200),
+        app.post("/charge").expect(200),
+        app.post("/charge").expect(200),
+        app.post("/charge").expect(200),
+        app.post("/charge").expect(200),
+        app.post("/charge").expect(200),
+        app.post("/charge").expect(200),
+        app.post("/charge").expect(200),
+        app.post("/charge").expect(200),
+    ]);
+    const afterTest = await app.post("/charge").expect(200);
+    assert.equal(afterTest.body.isAuthorized, false);
+    assert.equal(afterTest.body.remainingBalance, 0);
+    assert.equal(afterTest.body.charges, 0);
+}
 
 async function basicLatencyTest() {
     await app.post("/reset").expect(204);
@@ -17,6 +42,7 @@ async function basicLatencyTest() {
 
 async function runTests() {
     await basicLatencyTest();
+    await problemTest();
 }
 
 runTests().catch(console.error);
